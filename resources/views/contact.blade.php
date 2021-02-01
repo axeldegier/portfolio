8e9d4b0034dba28fae57e1278d8d31597fb9ceff
Axel de Gier <532600@student.glu.nl>
2021-02-01 15:28:55 +0100

    push

--- resources/views/contact.blade.php
+++ resources/views/contact.blade.php
@@ -17,9 +17,9 @@
 <!-- ======= Contact Section ======= -->
 <section id="contact" class="contact">
   <div class="container" data-aos="fade-up">
-
+   
     <div class="section-title">
-      <h2>Contact</h2>
+      <h2>Contact </h2>
       <p>Magnam dolores commodi suscipit. Necessitatibus eius consequatur ex aliquid fuga eum quidem. Sit sint consectetur velit. Quisquam quos quisquam cupiditate. Et nemo qui impedit suscipit alias ea. Quia fugiat sit in iste officiis commodi quidem hic quas.</p>
     </div>
 
@@ -35,19 +35,19 @@
           <div class="address">
             <i class="icofont-google-map"></i>
             <h4>Location:</h4>
-            <p>A108 Adam Street, New York, NY 535022</p>
+            <p></p>
           </div>
 
           <div class="email">
             <i class="icofont-envelope"></i>
             <h4>Email:</h4>
-            <p>info@example.com</p>
+            <p></p>
           </div>
 
           <div class="phone">
             <i class="icofont-phone"></i>
             <h4>Phonenumber:</h4>
-            <p>+1 5589 55488 55s</p>
+            <p></p>
           </div>
 
         </div>
